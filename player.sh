2659aa866e4f22a66e57c799c9aad3ac4bad3d62
skafiend <71149098+skafiend@users.noreply.github.com>
2025-04-04 20:57:35 +0300

    Update player.sh

--- player.sh
+++ player.sh
@@ -2,31 +2,38 @@
 
 _COLOR="%%{F$PRIMARY_COLOR}"
 COLOR_="%%{F-}"
-LEFT=""
-RIGHT=""
+MAX_TITLE=55
+ACTION="$1"
 
-_print_hours() {
-    printf ' %02d:%02d:%02d ' "$1" "$2" "$3"
+# time_in_seconds format (3/whatever)
+_time() {
+    if [ $2 == 3 ]; then
+        printf ' %02d:%02d:%02d ' "$(($1/3600))" "$(($1%3600/60))" "$(($1%60))"
+    fi
+
+    if [ $2 == 2 ]; then
+        printf ' %02d:%02d ' "$(($1%3600/60))" "$(($1%60))"
+    fi
 }
 
-_print_minutes() {
-    printf ' %02d:%02d ' "$1" "$2"
-}
 
 _trim_title() {
-    if [ ${#1} -gt 55 ]; then
+    if [ ${#1} -gt $MAX_TITLE ]; then
         TITLE="$(printf '%.55s' "$1")..."
     else
         TITLE="$1"
     fi
 }
 
+# action will work only when a $1 parameter for the script is not empty
+# for example config.ini:
+# click-left = ~/.config/polybar/player.sh play
 _run_action() {
     case $1 in
-        left)
-            eval $LEFT;;
-        right)
-            eval $RIGHT;;
+        play)
+            eval $PLAY;;
+        exit)
+            eval $EXIT;;
         up)
             eval $UP;;
         down)
@@ -34,17 +41,16 @@ _run_action() {
     esac
 }
 
-_getdata() {
+_get_data() {
     case $1 in
         mpv)
-        # you have to run mpv with a path to the socket to make it work
         MPV_SOCKET='/tmp/mpvsocket'
         TITLE=$(echo '{ "command": ["get_property", "media-title"] }' | socat - $MPV_SOCKET | jq -r .data)
         # in seconds
         POSITION=$(echo '{ "command": ["get_property_string", "time-pos"] }' | socat - $MPV_SOCKET  | jq -r .data | cut -d'.' -f 1)
         DURATION=$(echo '{ "command": ["get_property_string", "duration"] }' | socat - $MPV_SOCKET | jq -r .data | cut -d'.' -f 1)
-        LEFT="echo 'cycle pause' | socat - $MPV_SOCKET" 
-        RIGHT="echo 'quit' | socat - $MPV_SOCKET" 
+        PLAY="echo 'cycle pause' | socat - $MPV_SOCKET" 
+        EXIT="echo 'quit' | socat - $MPV_SOCKET" 
         UP="echo 'add volume +5' | socat - $MPV_SOCKET" 
         DOWN="echo 'add volume -5' | socat - $MPV_SOCKET" 
         ;;
@@ -55,41 +61,39 @@ _getdata() {
         TITLE="$ARTIST - $NAME"
         POSITION=$(cmus-remote -C status | grep "position" | cut -f 2 -d ' ')
         DURATION=$(cmus-remote -C status | grep "duration" | cut -f 2 -d ' ')
-        LEFT="cmus-remote --pause"
-        RIGHT="pkill -x cmus"
+        PLAY="cmus-remote --pause"
+        EXIT="pkill -x cmus"
         UP="cmus-remote --volume +5%"
         DOWN="cmus-remote --volume -5%"
         ;;
     esac
 }
 
-# name position duration
-_display_data() {
+# time_format (Number)
+_compose_string() {
     # ' - ' - cmus stopped and we didn't get any info
-    if [[ ! -z "$1" && "$1" != ' - ' ]]; then
-        printf "$_COLOR$1$COLOR_" 
-        $2
+    if [[ ! -z "$TITLE" && "$TITLE" != ' - ' ]]; then
+        printf "$_COLOR$TITLE$COLOR_" 
+        _time $POSITION $1
         printf "$_COLOR/$COLOR_"
-        $3
+        _time $DURATION $1
         printf "$_COLOR|$COLOR_"
     else
         echo ""
     fi
 }
 
+# player_name time_format (Number)
+_display() {
+    _get_data $1 
+    _trim_title "$TITLE"
+    _compose_string $2
+    _run_action $ACTION
+}
+
 # assume: only one thing is running at a time
 if pgrep -f mpv_audiobook > /dev/null; then
-    _getdata mpv 
-    _trim_title "$TITLE"
-    _display_data "$TITLE" \
-                 "_print_hours $((POSITION/3600)) $((POSITION%3600/60)) $((POSITION%60))" \
-                 "_print_hours $((DURATION/3600)) $((DURATION%3600/60)) $((DURATION%60))"
-    _run_action $1
+    _display mpv 3
 else
-    _getdata cmus
-    _trim_title "$TITLE"
-    _display_data "$TITLE"  \
-                  "_print_minutes $((POSITION%3600/60)) $((POSITION%60))" \
-                  "_print_minutes $((DURATION%3600/60)) $((DURATION%60))"
-    _run_action $1
+    _display cmus 2
 fi
